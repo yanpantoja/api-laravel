95e365de3f739826420981886dfd080170caf5cd
Yan Pantoja <yanmpantoja@gmail.com>
2019-11-04 09:31:27 -0300

    first commit

---
+++ database/migrations/2019_11_01_181214_create_clientes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome', 150);
+            $table->string('image', 100)->nullable();
+            $table->string('cpf_cnpj', 19)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
