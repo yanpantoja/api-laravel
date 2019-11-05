229e7773917b876c30f8f612f6babb5f119c9363
Yan Pantoja <yanmpantoja@gmail.com>
2019-11-05 15:29:10 -0300

    crud methods ok

--- app/Http/Controllers/Api/ClienteApiController.php
+++ app/Http/Controllers/Api/ClienteApiController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
+use Intervention\Image\ImageManagerStatic as Image;
+use Illuminate\Support\Facades\Storage;
+
 use App\Http\Controllers\Controller;
 use App\Models\Cliente;
+
 
 class ClienteApiController extends Controller
 {
@@ -12,13 +16,13 @@ class ClienteApiController extends Controller
     public function __construct(Cliente $cliente, Request $request)
     {
         $this->cliente = $cliente;
-        $this->request = $request;    
+        $this->request = $request;
     }
-    
+
 
     public function index()
     {
-        $data = $this->cliente->all(); 
+        $data = $this->cliente->all();
         return response()->json($data);
     }
 
@@ -32,6 +36,22 @@ class ClienteApiController extends Controller
         $this->validate($request, $this->cliente->rules());
 
         $dataForm = $request->all();
+
+        if($request->hasFile('image') && $request->file('image')->isValid()){
+            $ext = $request->image->extension();
+
+            $name = uniqid(date('His'));
+
+            $nameFile = "{$name}.{$ext}";
+
+            $upload = Image::make($dataForm['image'])->resize(177,236)->save(\storage_path("app/public/clientes/$nameFile", 70));
+
+            if(!$upload){
+                return response()->json(['error' => 'Falha ao fazer upload'], 500);
+            }else{
+                $dataForm['image'] = $nameFile;
+            }
+        }
         $data = $this->cliente->create($dataForm);
 
         return response()->json($data, 201);
@@ -39,7 +59,12 @@ class ClienteApiController extends Controller
 
     public function show($id)
     {
-        //
+        $data = $this->cliente->find($id);
+        if($data){
+            return response()->json($data);
+        }else{
+            return response()->json(["error" => "id does not exist"], 404);
+        }
     }
 
     public function edit($id)
@@ -49,11 +74,46 @@ class ClienteApiController extends Controller
 
     public function update(Request $request, $id)
     {
-        //
+        if(!$data = $this->cliente->find($id))
+            return response()->json(["error" => "id does not exist"], 404);
+
+
+        $dataForm = $request->all();
+
+        if($request->hasFile('image') && $request->file('image')->isValid()){
+
+            if($data->image){
+                Storage::disk('public')->delete("/clientes/$data->image");
+            }
+
+            $ext = $request->image->extension();
+
+            $name = uniqid(date('His'));
+
+            $nameFile = "{$name}.{$ext}";
+
+            $upload = Image::make($dataForm['image'])->resize(177,236)->save(\storage_path("app/public/clientes/$nameFile", 70));
+
+            if(!$upload){
+                return response()->json(['error' => 'Falha ao fazer upload'], 500);
+            }else{
+                $dataForm['image'] = $nameFile;
+            }
+        }
+
+        $data->update($dataForm);
+
+        return response()->json($data);
     }
 
     public function destroy($id)
     {
-        //
+        if(!$data = $this->cliente->find($id))
+            return response()->json(["error" => "id does not exist"], 404);
+        if($data->image){
+            Storage::disk('public')->delete("/clientes/$data->image");
+        }
+        $data->delete();
+        return response()->json(['success' => "successfuly deleted"]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,9 @@ use Illuminate\Http\Request;
 */
 
 Route::get('clientes/' , 'Api\ClienteApiController@index');
+Route::get('clientes/{id}' , 'Api\ClienteApiController@show');
+
 Route::post('clientes/' , 'Api\ClienteApiController@store');
+Route::put('clientes/{id}' , 'Api\ClienteApiController@update');
+
+Route::delete('clientes/{id}' , 'Api\ClienteApiController@destroy');
